354a4fbdfa5f2438145f99a4e8bd71417b6632b3
redundant4u <rafch@naver.com>
2021-07-08 09:07:59 +0900

    [S] Initialize memory for IA-32e mode

--- volumes/01.Kernel32/Source/Main.c
+++ volumes/01.Kernel32/Source/Main.c
@@ -1,10 +1,16 @@
 #include "Types.h"
 
 void kPrintString(int iX, int iY, const char* pcString);
+BOOL kInitiakizeKernel64Area(void);
 
 void Main(void)
 {
+    DWORD i;
+
     kPrintString(0, 3, "C Language Kernel Start");
+
+    kInitiakizeKernel64Area();
+    kPrintString(0, 4, "IA-32e Kernel Area Initialization Complete");
     while(1);
 }
 
@@ -17,4 +23,25 @@ void kPrintString(int iX, int iY, const char* pcString)
     for( i = 0; pcString[i] != 0; i++) {
         pstScreen[i].bCharactor = pcString[i];
     }
+}
+
+BOOL kInitiakizeKernel64Area(void)
+{
+    DWORD* pdwCurrentAddress;
+
+    pdwCurrentAddress = (DWORD*) 0x100000;
+
+    while((DWORD) pdwCurrentAddress < 0x60000)
+    {
+        *pdwCurrentAddress = 0x00;
+
+        if(*pdwCurrentAddress != 0)
+        {
+            return FALSE;
+        }
+
+        pdwCurrentAddress++;
+    }
+
+    return TRUE;
 }
